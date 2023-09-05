a6021d90fd91efd55a2c0d0cbae0a0357b2ba76e
battlebyte <jordi@fernandez.name>
2023-09-05 11:49:14 +0200

    New command "deck file kong2kic".

---
+++ cmd/file_kong2kic.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kong/deck/file"
+	"github.com/kong/go-apiops/logbasics"
+	"github.com/spf13/cobra"
+)
+
+var (
+	cmdKong2KicInputFilename  string
+	cmdKong2KicOutputFilename string
+	cmdKong2KicApi            string
+)
+
+// Executes the CLI command "kong2kic"
+func executeKong2Kic(cmd *cobra.Command, _ []string) error {
+
+	var (
+		outputContent    *file.Content
+		err              error
+		outputFileFormat file.Format
+	)
+
+	verbosity, _ := cmd.Flags().GetInt("verbose")
+	logbasics.Initialize(log.LstdFlags, verbosity)
+
+	inputContent, err := file.GetContentFromFiles([]string{cmdKong2KicInputFilename})
+	if err != nil {
+		return fmt.Errorf("failed reding input file '%s'; %w", cmdKong2KicInputFilename, err)
+	}
+
+	outputContent = inputContent.DeepCopy()
+	outputFileFormat = file.KIC
+
+	err = file.WriteContentToFile(outputContent, cmdKong2KicOutputFilename, outputFileFormat)
+
+	if err != nil {
+		return fmt.Errorf("failed converting Kong to Ingress '%s'; %w", cmdKong2KicInputFilename, err)
+	}
+
+	return nil
+}
+
+//
+//
+// Define the CLI data for the openapi2kong command
+//
+//
+
+func newKong2KicCmd() *cobra.Command {
+	kong2KicCmd := &cobra.Command{
+		Use:   "kong2kic",
+		Short: "Convert Kong configuration files to Kong Ingress Controller (KIC) manifests",
+		Long: `Convert Kong configuration files to Kong Ingress Controller (KIC) manifests
+		
+Currently, only ingress controller manifests are supported. Output format is always YAML.`,
+		RunE: executeKong2Kic,
+		Args: cobra.NoArgs,
+	}
+
+	kong2KicCmd.Flags().StringVarP(&cmdKong2KicInputFilename, "input-file", "i", "-",
+		"Kong spec file to process. Use - to read from stdin.")
+	kong2KicCmd.Flags().StringVarP(&cmdKong2KicOutputFilename, "output-file", "o", "-",
+		"Output file to write. Use - to write to stdout.")
+	kong2KicCmd.Flags().StringVarP(&cmdKong2KicApi, "api", "a", "ingress", "[ingress|gateway]")
+
+	return kong2KicCmd
+}
---
+++ file/kong2kic_test.go
@@ -0,0 +1,83 @@
+package file
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_convertKongGatewayToIngress(t *testing.T) {
+	type args struct {
+		inputFilename  string
+		outputFilename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "convert one service",
+			args: args{
+				inputFilename:  "testdata/kong2kic/1/input.yaml",
+				outputFilename: "testdata/kong2kic/1/output-expected.yaml",
+			},
+			wantErr: false,
+		},
+		{
+			name: "convert one service with upstream",
+			args: args{
+				inputFilename:  "testdata/kong2kic/2/input.yaml",
+				outputFilename: "testdata/kong2kic/2/output-expected.yaml",
+			},
+			wantErr: false,
+		},
+		{
+			name: "convert one service with upstream and route",
+			args: args{
+				inputFilename:  "testdata/kong2kic/3/input.yaml",
+				outputFilename: "testdata/kong2kic/3/output-expected.yaml",
+			},
+			wantErr: false,
+		},
+		{
+			name: "convert one service with upstream, route and consumer",
+			args: args{
+				inputFilename:  "testdata/kong2kic/4/input.yaml",
+				outputFilename: "testdata/kong2kic/4/output-expected.yaml",
+			},
+			wantErr: false,
+		},
+		{
+			name: "convert one service with upstream, route-plugins, consumer",
+			args: args{
+				inputFilename:  "testdata/kong2kic/5/input.yaml",
+				outputFilename: "testdata/kong2kic/5/output-expected.yaml",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			inputContent, err := GetContentFromFiles([]string{tt.args.inputFilename})
+			if err != nil {
+				assert.Fail(t, err.Error())
+			}
+
+			output, err := KongToKIC(inputContent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("KongToKIC() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if err == nil {
+
+				expected, err := os.ReadFile(tt.args.outputFilename)
+				if err != nil {
+					assert.Fail(t, err.Error())
+				}
+				assert.Equal(t, string(expected), string(output))
+			}
+		})
+	}
+}
